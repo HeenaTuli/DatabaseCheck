7cc37683f7ab46ad86afe411c965f1e7414a2bb1
HeenaTuli <heenabajaj08@gmail.com>
2016-12-05 14:17:20 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 mysql -h "testcodeship.c3pmyiqsuzhg.us-east-1.rds.amazonaws.com" -u "admin" "-pCodeship" << EOF
 #mysql -h testcodeship.c3pmyiqsuzhg.us-east-1.rds.amazonaws.com -u admin -pCodeship << EOF
 show DATABASES;
-USE "TestCodeship";
+SELECT DATABASE();
 show tables;
 EOF 
