c97b22d55977a43e94fcb0507eb9860438b5a6a7
HeenaTuli <heenabajaj08@gmail.com>
2016-12-05 13:58:31 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
-# Shell script to run sql files from command line.
-# Pre-Req: sqlplus client shall be installed already.
-###########################################################
-# Variables Section (DB Details)
-###########################################################
-DB_HostName="testcodeship.c3pmyiqsuzhg.us-east-1.rds.amazonaws.com"
-DB_Port="3306"
-DB_SID="testcodeship"
-DB_UserName="admin"
-DB_Password="Codeship"
-DIR_SqlFiles="/home/rof/src/github.com/HeenaTuli/DatabaseCheck/"
-##########################################################
-# All Script Functions Goes Here
-##########################################################
-
-echo "`date` :Checking DB connectivity...";
-echo "`date` :Trying to connect "${DB_UserName}"/"${DB_Password}"@"${DB_SID}" ..."
-#echo "exit" | mysql "${DB_UserName}/${DB_Password}@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=${DB_HostName})(PORT=${DB_Port})))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=${DB_SID})))" | grep -q "Connected to:" > /dev/null
-mysql -h testcodeship.c3pmyiqsuzhg.us-east-1.rds.amazonaws.com -P 3306 -u admin -pCodeship < /home/rof/src/github.com/HeenaTuli/DatabaseCheck/createtable.sql;
-exit(); 
-exit 
-
+mysql -h testcodeship.c3pmyiqsuzhg.us-east-1.rds.amazonaws.com -P 3306 -u admin -pCodeship << EOF
+use mysql;
+show tables;
+EOF 
