bcdda8aad551938fb86549f4a38fbeff8e168789
HeenaTuli <heenabajaj08@gmail.com>
2016-12-05 15:08:45 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,4 +8,4 @@ SELECT DATABASE();
 show tables;
 #CREATE TABLE TestCodeship.tutorials_tbl(tutorial_id INT NOT NULL AUTO_INCREMENT,tutorial_title VARCHAR(100) NOT NULL,tutorial_author VARCHAR(40) NOT NULL,submission_date DATE,PRIMARY KEY ( tutorial_id ));
 #INSERT INTO TestCodeship.tutorials_tbl (tutorial_title,tutorial_author, submission_date) VALUES ('codeship_database','heena','25-06-2016') ;
-mysql -e | cat /home/rof/src/github.com/HeenaTuli/DatabaseCheck/select.sql;
+cat /home/rof/src/github.com/HeenaTuli/DatabaseCheck/select.sql;
