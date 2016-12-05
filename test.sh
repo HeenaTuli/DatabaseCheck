96ae52deb6a4b401071b22b7a96dfd95efb6e08e
HeenaTuli <heenabajaj08@gmail.com>
2016-12-05 14:57:11 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ USE TestCodeship
 SELECT DATABASE();
 show tables;
 #CREATE TABLE TestCodeship.tutorials_tbl(tutorial_id INT NOT NULL AUTO_INCREMENT,tutorial_title VARCHAR(100) NOT NULL,tutorial_author VARCHAR(40) NOT NULL,submission_date DATE,PRIMARY KEY ( tutorial_id ));
-SELECT * from tutorials_tb1 ;
+SELECT * from TestCodeship.tutorials_tbl;
