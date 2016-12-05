7eb855319b01d0d2ca2d674bc6ed72f11a43612e
HeenaTuli <heenabajaj08@gmail.com>
2016-12-05 13:40:14 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -16,18 +16,8 @@ DIR_SqlFiles="/home/rof/src/github.com/HeenaTuli/DatabaseCheck/"
 
 echo "`date` :Checking DB connectivity...";
 echo "`date` :Trying to connect "${DB_UserName}"/"${DB_Password}"@"${DB_SID}" ..."
-echo "exit" | mysql "${DB_UserName}/${DB_Password}@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=${DB_HostName})(PORT=${DB_Port})))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=${DB_SID})))" | grep -q "Connected to:" > /dev/null
-if [ $? -eq 0 ]
-then
-DB_STATUS="UP"
-export DB_STATUS
-echo "`date` :Status: ${DB_STATUS}. Able to Connect..."
-else
-DB_STATUS="DOWN"
-export DB_STATUS
-echo "`date` :Status: DOWN . Not able to Connect."
-echo "`date` :Exiting Script Run..."
-exit
-fi
-
+#echo "exit" | mysql "${DB_UserName}/${DB_Password}@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=${DB_HostName})(PORT=${DB_Port})))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=${DB_SID})))" | grep -q "Connected to:" > /dev/null
+mysql -h testcodeship.c3pmyiqsuzhg.us-east-1.rds.amazonaws.com -P 3306 -u admin -pCodeship ;
+exit(); 
+exit 
 
