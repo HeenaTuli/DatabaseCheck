0ef1241b703d9246ab69df566046771a76bfeba5
HeenaTuli <heenabajaj08@gmail.com>
2016-12-05 14:02:09 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mysql -h testcodeship.c3pmyiqsuzhg.us-east-1.rds.amazonaws.com -P 3306 -u admin -pCodeship << EOF
-use mysql;
+mysql -h testcodeship.c3pmyiqsuzhg.us-east-1.rds.amazonaws.com -u admin -pCodeship << EOF
+use testcodeship;
 show tables;
 EOF 
